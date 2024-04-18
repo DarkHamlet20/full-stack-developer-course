d0a1c8299a8711d49a5fa6c1a765966cb81e8753
DarkHamlet20 <149280380+DarkHamlet20@users.noreply.github.com>
2024-04-17 21:04:57 -0400

    add structure with your styles and a button component

--- react-projects/calculator/src/App.js
+++ react-projects/calculator/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
-
+import "./App.css";
+import talendingLogo from "../src/img/talendingLogo.png";
+import Boton from "../src/components/Boton"
 function App() {
   return (
     <div className="App">
-      
+      <div className="talending-logo-container">
+        <img
+          src={talendingLogo}
+          className="talending-logo"
+          alt="Logo de talending"
+        />
+      </div>
+      <div className="container-calculator">
+        <div className="">
+          <div className="fila">
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+
+          </div>
+          <div className="fila"></div>
+          <div className="fila"></div>
+          <div className="fila"></div>
+          <div className="fila"></div>
+        </div>
+      </div>
     </div>
   );
 }
